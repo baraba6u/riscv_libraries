faac8a91f5873a7baddef023a6b43d6e9de9e5d4
Ungsang Yun <baraba6u@snu.ac.kr>
2023-10-28 17:09:27 +0200

    initial commit - including rv32emc / rv32imc

---
+++ libgcc/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/ns3/gcc/riscv-gnu-toolchain/gcc/configure --target=riscv64-unknown-elf --prefix=/home/ns3/gcc/gcc13 --disable-shared --disable-threads --enable-languages=c,c++ --with-pkgversion=gc891d8dc23e --with-system-zlib --enable-tls --with-newlib --with-sysroot=/home/ns3/gcc/gcc13/riscv64-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=.././gcc --enable-multilib --with-multilib-generator='rv32e-ilp32e--;rv32ec-ilp32e--;rv32em-ilp32e--;rv32emc-ilp32e--;rv32i-ilp32--;rv32ic-ilp32--;rv32im-ilp32--;rv32imc-ilp32--' --with-abi=lp64d --with-arch=rv64imafdc --with-tune=rocket --with-isa-spec=20191213 'CFLAGS_FOR_TARGET=-Os    -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-Os    -mcmodel=medlow'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafdc_zicsr" }, { "tune", "rocket" }, { "isa_spec", "20191213" } };
